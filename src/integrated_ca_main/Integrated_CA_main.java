622a63023daae3c57d4a602c12a95ff1b8c99a0c
Kevin S. <146198846+2022271-KAS@users.noreply.github.com>
2023-12-20 00:51:38 +0000

    First commit, test.

---
+++ src/integrated_ca_main/Integrated_CA_main.java
@@ -0,0 +1,17 @@
+package integrated_ca_main;
+/**
+ *Year 2 Integrated CA OOC/DB
+ * Github: https://github.com/2022271-KAS/Integrated_CA
+ * Kevin De Araujo Sanchez - 2022271
+ * Marccus Winicius Nunes Santos - 2022275
+ */
+public class Integrated_CA_main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
