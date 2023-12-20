7709dde5cbc8158d5757dd6b19ce895ce7974277
Kevin S. <146198846+2022271-KAS@users.noreply.github.com>
2023-12-20 01:52:29 +0000

    Created the ADMIN class that will handle all admin's related activities. Initialized the DbWriter so we can use some of its methods. Also updated and imported IOUtils.

---
+++ src/integrated_ca_main/Admin.java
@@ -0,0 +1,17 @@
+package integrated_ca_main;
+
+import ioutils.IOUtils;
+
+
+//This is the Admin class that will handle all admin's activities related. 
+public class Admin {
+    
+    //It declares a constant reference to the instance of the dbWriter class so I can use it in this class.
+    //It needs to be initialized.
+    private final DbWriter writer = new DbWriter();
+
+    //IOUtils to let the admin input.
+    IOUtils myInput = new IOUtils();
+    //Method for the adminMenu.
+    
+}
