6751c67fd093b7e2a83950b6db797ae99a76d0ec
Kevin S. <146198846+2022271-KAS@users.noreply.github.com>
2023-12-20 02:29:11 +0000

    CODDED the mainmenu part of the code. The main menu WILL ONLY give 3 options. 1-Login. 2-Register. 3-Exit. The menu is CONTORLLED BY A LOOP using a boolean variable to keep it going as long as it's false. The database is always INITIALIZED when the code runs, a successful or failure message will be display averytime. The menu will let the user LOGIN as the ADMINn or as a REGULAR USER and depending on what type of user is logged a different MENU will be accessed. Either the admin menu or the regular user menu.

--- src/integrated_ca_main/Integrated_CA_main.java
+++ src/integrated_ca_main/Integrated_CA_main.java
@@ -1,4 +1,7 @@
 package integrated_ca_main;
+
+import ioutils.IOUtils;
+
 /**
  *Year 2 Integrated CA OOC/DB
  * Github: https://github.com/2022271-KAS/Integrated_CA_main
@@ -10,15 +13,86 @@ public class Integrated_CA_main {
     
     //Main code, where all the pieces come together.
     //Here we also throw all the necessary exceptions.
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         
+       //It initializes the database by calling the setupDB method.
+        if (DbSetUp.setupDB()) {
+            //Success message.
+            System.out.println("Database setup successful.");
+            System.out.println();
+        } else {
+            //Error message. 
+            System.out.println("Database setup failed.");
+        }
 
-        
-        //Try catch to run the main menu.
-        try{
-        
-            
-        }catch(Exception e){}
+        //Try catch to run the main menu. 
+        try {
+
+            //Ioutils to collect user's input.
+            IOUtils myInput = new IOUtils();
+            //Boolean variable to control for how long the menu will keep on being looped.
+            boolean leave = false;
+
+            //Loop for the menu, it will keep looping until the variable leave changes to true. 
+            //The idea of the main menu is to give 3 options, 1 to log-in in case the user already have an account. 
+            //2 for registration and 3 to exit. 
+            do {
+                //Variable that will decide how the program will respond depending on the user's choice.
+                int command = myInput.getUserInt("Welcome to the tax calculator! You need to log-in to use it. \n1-Login \n2-Register \n3-Exit");
+
+                //Switch to work with the menu.
+                switch (command) {
+
+                    //Case 1 allows users to login. Both the administrator and regular users.
+                    case 1:
+                        //It uses the LoginAuthenticator class to handle the login proccess. 
+                        LoginAuthenticator.handleLogin();
+                        System.out.println();
+                        break;
+
+                    //Case 2 allows to register a new user.
+                    case 2:
+
+                        //It initializes the dbWriter so we can write into the database.
+                        DbWriter dbw = new DbWriter();
+                        //It calls the createUser method to create a new user.
+                        if (dbw.createUser()) {
+                            //If it succeeds a message will confirm it. 
+                            System.out.println("User added successfully!");
+                        } else {
+                            //If it fails a message will be displayed as well.
+                            System.out.println("Failed to add user.");
+                        }
+                        System.out.println();
+                        break;
+
+                    //Case 3 exits the program. 
+                    case 3:
+                        System.out.println("Thanks for using the tax calculator!");
+                        //It changes the state of the boolean variable leave to true and the loop ends.
+                        leave = true;
+                        System.out.println();
+                        break;
+
+                    //Default error message.
+                    default:
+                        System.out.println("Oops! Select available options only.");
+                        System.out.println();
+                }
+
+                //The loop will continue as long as leave is false.
+            } while (!leave);
+
+        //Catch to avoid the code from crashing.
+        } catch (Exception e) {
+            //Error message specifying what part of the code went wrong. 
+            System.out.println("MainMenu - Something went very bad!");
+            System.out.println();
+            //Error tracer.
+            e.printStackTrace();
+        //Finally, last message to the user.
+        } finally {
+            System.out.println("Have a great day!");
+        }
     }
-    
 }
