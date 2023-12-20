204b7b1e8616365490f9af3fa451fd4dd6f16353
Kevin S. <146198846+2022271-KAS@users.noreply.github.com>
2023-12-20 01:29:07 +0000

    Added the last method of the DbSetUp class. The method is used to drop the whole schema.

--- src/integrated_ca_main/Integrated_CA_main.java
+++ src/integrated_ca_main/Integrated_CA_main.java
@@ -1,10 +1,11 @@
 package integrated_ca_main;
 /**
  *Year 2 Integrated CA OOC/DB
- * Github: https://github.com/2022271-KAS/Integrated_CA
+ * Github: https://github.com/2022271-KAS/Integrated_CA_main
  * Kevin De Araujo Sanchez - 2022271
  * Marccus Winicius Nunes Santos - 2022275
  */
+
 public class Integrated_CA_main {
     
     //Main code, where all the pieces come together.
